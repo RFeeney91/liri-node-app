7d45991f4e0c2c0816b2a12a63c6962429a3f6d7
RFeeney91 <ryansfeeney@gmail.com>
2017-10-21 07:49:05 -0700

    Update liri.js

--- liri.js
+++ liri.js
@@ -49,3 +49,19 @@ fs.readFile("random.txt", "utf8", function(error, data) {
   console.log(dataArr);
 
 });
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: <your spotify client id>,
+  secret: <your spotify client secret>
+});
+ 
+spotify
+  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+  .then(function(data) {
+    console.log(data); 
+  })
+  .catch(function(err) {
+    console.error('Error occurred: ' + err); 
+  });
