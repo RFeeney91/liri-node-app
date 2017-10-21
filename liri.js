4aaeaac3343f3aa967eb9cf626ffb5053da0dd7e
RFeeney91 <ryansfeeney@gmail.com>
2017-10-21 07:39:27 -0700

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,4 +1,16 @@
 var request = require("./keys.js");
+
+request("https://api.twitter.com/1.1/statuses/show.json", function(error, response, body) {
+
+  // If the request is successful (i.e. if the response status code is 200)
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("Tweets");
+  }
+});
+
 // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
 var request = require("request");
 
