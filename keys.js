bbb9bbbaf4f6bd1a8a7798b0996024e196b05cc4
RFeeney91 <ryansfeeney@gmail.com>
2017-10-21 07:19:23 -0700

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'iLIQrfSGNNYL2OKHLhVHybUn4',
+  consumer_secret: 'guAHIattfNxVFcICiPyLFzKdjGMslIjz6my8PDHOGEbwIUgyuc',
+  access_token_key: '920093555032330241-jO1lXcIe4D5OmE4MEEjPRLe24wTsfN4',
+  access_token_secret: 'EdHP154qcvk5Us36bvu6A8QPInuWzl3y8domqSw5e5kLP',
+}
+
+module.exports = twitterKeys;
